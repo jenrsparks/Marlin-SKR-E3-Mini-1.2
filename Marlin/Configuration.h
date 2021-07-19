44913ba4d326403195879f82f6f7c1d7af77f4e8
Jen Sparks <1487084+jenrsparks@users.noreply.github.com>
2021-07-18 19:23:12 -0500

    Updated for new config of direct drive remount

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -923,7 +923,7 @@
  *
  * Specify a Probe position as { X, Y, Z }
  */
-#define NOZZLE_TO_PROBE_OFFSET { -44, -14, -0 }
+#define NOZZLE_TO_PROBE_OFFSET { -43, -13, -2.3 }
 
 // Certain types of probes need to stay away from edges
 #define MIN_PROBE_EDGE 10
